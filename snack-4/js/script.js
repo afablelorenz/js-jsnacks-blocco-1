c9c0de1947a9a8e28641fc88bf0df783b341ebae
afablelorenz <afablelorenz@gmail.com>
2024-05-13 12:14:16 +0200

    finished script js

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -2,6 +2,13 @@ const invitati = ['marco','davide','michele','martina','federico'];
 let nomeUtente;
 
 nomeUtente=prompt('inserire il suo nome');
-for(let i;i<invitati.length();i++){
-    
+
+for(let i=0;i<invitati.length;i++){
+    if(invitati[i]===nomeUtente){
+        console.log('Lei è invitato');
+        break;
+    }else{
+        console.log('Non è invitato');
+        break;
+    }
 }
