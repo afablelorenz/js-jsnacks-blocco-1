e63b303b702b8c95e61194351ce22cce546802db
afablelorenz <afablelorenz@gmail.com>
2024-05-13 11:51:18 +0200

    updated

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,8 +1,8 @@
 let num1;
 let num2;
 
-num1=Number.parseInt(prompt('Inserisci un numero'));
-num2=Number.parseInt(prompt('Inserisci un numero'));
+num1=Number.parseInt(prompt('Inserisci un numero'),10);
+num2=Number.parseInt(prompt('Inserisci un numero'),10);
 
 if(isNaN(num1) || isNaN(num2)){
     console.log('inserire numeri validi');
