c5f7f6dcc0b79568c51e3bafede37f5b972dacd4
afablelorenz <afablelorenz@gmail.com>
2024-05-13 12:20:38 +0200

    correzione esercizio

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -2,13 +2,24 @@ const invitati = ['marco','davide','michele','martina','federico'];
 let nomeUtente;
 
 nomeUtente=prompt('inserire il suo nome');
+nomeUtente= nomeUtente.charAt(0).toUpperCase()+nomeUtente.substring(nomeUtente.length).toLowerCase();
 
-for(let i=0;i<invitati.length;i++){
-    if(invitati[i]===nomeUtente){
-        console.log('Lei è invitato');
-        break;
-    }else{
-        console.log('Non è invitato');
-        break;
-    }
+if(invitati.includes(nomeUtente)){
+    console.log('è invitato');
+}else{
+    console.log('non è invitato');
 }
+
+
+/*for(let i=0;i<invitati.length;i++){
+    if(invitati[i]===nomeUtente){
+        console.log(invitati[i]);
+        console.log(nomeUtente);
+        console.log('Lei è invitato');
+    }else{
+        console.log(invitati[i]);
+        console.log(nomeUtente);
+        console.log('Non è invitato');
+    }
+}*/
+
