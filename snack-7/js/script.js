94063695ca022f7b7759b583852a4a0c5a3f8838
afablelorenz <afablelorenz@gmail.com>
2024-05-14 11:13:26 +0200

    updated script(math.random +1)

--- snack-7/js/script.js
+++ snack-7/js/script.js
@@ -4,7 +4,7 @@ n=Number.parseInt(prompt('inserire numero'));
 for(let i=0; i<n;i++){
     let arr = [];
     for(let j=0; j<10;j++){
-        arr.push(Math.floor(Math.random()*100));
+        arr.push(Math.floor(Math.random()*100)+1);
     }
     console.log(arr);
 }
